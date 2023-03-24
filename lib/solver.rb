b3dda59f15a9965d5fbc83461c585a95962459dd
peter <peterugwu64@gmail.com>
2023-03-24 10:06:10 +0100

    Update solver.rb file

--- lib/solver.rb
+++ lib/solver.rb
@@ -1,31 +1,31 @@
 class Solver
-    def factorial(num)
-      raise 'Cannot factorize negative numbers' if num.negative?
-  
-      if num.zero?
-        1
-      else
-        result = 1
-        (1..num).each do |number|
-          result *= number
-        end
-        result
+  def factorial(num)
+    return 'Incorrect input. Param must be >= 0' if num.negative?
+
+    if num.zero?
+      1
+    else
+      result = 1
+      (1..num).each do |number|
+        result *= number
       end
-    end
-  
-    def reverse(str)
-      str.reverse
-    end
-  
-    def fizzbuzz(num)
-      if (num % 3).zero? && (num % 5).zero?
-        'fizzbuzz'
-      elsif (num % 5).zero?
-        'buzz'
-      elsif (num % 3).zero?
-        'fizz'
-      else
-        num.to_s
-      end
+      result
     end
   end
+
+  def reverse(str)
+    str.reverse
+  end
+
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 5).zero?
+      'buzz'
+    elsif (num % 3).zero?
+      'fizz'
+    else
+      num.to_s
+    end
+  end
+end
